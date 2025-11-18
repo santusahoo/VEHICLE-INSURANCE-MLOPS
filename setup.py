0e86688124b7cf0060b69922092ef836304c75e6
Santusahoo <sahoosantu79@gmail.com>
2025-11-19 00:44:03 +0530

    last commit ready for deployment

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Santu Sahoo",
+    author_email="sahoosantu79@gmail.com",
+    packages=find_packages()
+)
